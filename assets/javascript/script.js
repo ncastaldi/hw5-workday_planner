4a49ee8f18e3d36fae8128e2b6408658c468233e
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-16 16:48:31 -0500

    created function to create and display hour blocks on page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,8 +13,39 @@ $(document).ready(function () {
         dayDisplayEl.text(dayDisplay);
     }
 
+    function displayTimeBlocks() {
+        for(var i=0; i < workdayHours.length; i++){
+            var rowEl = $("<row>");
+            rowEl.addClass("row time-block");
+
+            var hourColEl = $("<div>");
+            hourColEl.addClass("col-1 hour");
+            hourColEl.text(moment().set('hour', workdayHours[i]).format("hA"));
+            rowEl.append(hourColEl);
+
+            var descriptionColEl = $("<textarea>");
+            descriptionColEl.addClass("col-10 past");
+            if (moment().hour() > workdayHours[i]) {
+                descriptionColEl.addClass("past");
+            } else if (moment().hour() < workdayHours[i]) {
+                descriptionColEl.addClass("future");
+            } else if (moment().hour() === workdayHours[i]) {
+                descriptionColEl.addClass("present");
+            }
+
+            rowEl.append(descriptionColEl);
+
+            var buttonColEl = $("<button>");
+            buttonColEl.addClass("col-1 saveBtn fas fa-save");
+            rowEl.append(buttonColEl);
+
+            containerEl.append(rowEl);
+        }
+    }
+
     /* Function Calls */
     displayDay();
+    displayTimeBlocks();
 
-    /* Event Listeners */
+    /* Define Event Listeners */
 })
