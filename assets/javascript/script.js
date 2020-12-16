57d8fee30854a0fb4f170c34046a91e930791b03
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-16 16:23:53 -0500

    created DOM variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,17 @@
-var now = moment();
-console.log(now);
+$(document).ready(function() {
+
+    /* Declare DOM Variables */
+    var dayDisplayEl = $("#currentDay");
+    var containerEl = $(".container");
+
+    /* Declare JS Variables */
+
+
+    /* Function Definitions */
+
+
+    /* Function Calls */
+
+
+    /* Event Listeners */
+})
