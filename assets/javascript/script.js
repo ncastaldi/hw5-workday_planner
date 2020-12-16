902cd1a1add65c41f57fdfded1373d6e20ce9df1
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-16 16:25:01 -0500

    created JS variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     /* Declare DOM Variables */
     var dayDisplayEl = $("#currentDay");
     var containerEl = $(".container");
 
     /* Declare JS Variables */
-
+    var dayDisplay = moment().format("dddd, MMMM Do");
+    var workdayHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
     /* Function Definitions */
 
